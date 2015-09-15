75b800fd9487cc09828c5313817dbb0890af57cf
Espen Ekvang <espen.ekvang@bekk.no>
2015-09-15 20:14:28 +0200

    Add MemberController

---
+++ Bekk.Kodehåndverk.Versioning/Controllers/MembersController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace Bekk.Kodehåndverk.Versioning.Controllers
+{
+    public class MembersController : BaseApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok(new { Name= "A member"});
+        }
+    }
+}
