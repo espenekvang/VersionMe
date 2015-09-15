06a88cf6f980138f1369b38bcbe0750eb1b58823
Espen Ekvang <espen.ekvang@bekk.no>
2015-09-15 21:06:49 +0200

    Add repository to return different models

---
+++ Bekk.Kodehåndverk.Versioning/Respositories/MemberRepository.cs
@@ -0,0 +1,24 @@
+﻿using Bekk.Kodehåndverk.Versioning.Models.V1;
+using Bekk.Kodehåndverk.Versioning.Models.V2;
+
+namespace Bekk.Kodehåndverk.Versioning.Respositories
+{
+    public class MemberRepository : IMemberRepository
+    {
+        public MemberModel Get(int memberId)
+        {
+            return new MemberModel {FirstName = "First"};
+        }
+
+        public MemberModelV2 GetV2(int memberId)
+        {
+            return new MemberModelV2 {FirstName = "First", LastName = "Last"};
+        }
+    }
+
+    public interface IMemberRepository
+    {
+        MemberModel Get(int memberId);
+        MemberModelV2 GetV2(int memberId);
+    }
+}
